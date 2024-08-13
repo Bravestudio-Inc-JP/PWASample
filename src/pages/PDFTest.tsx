3b9501445bd4eebf29d03bdd5a71c2884adbcf74
Bs-Kevin <z.kyaw@bravestudio.co.jp>
2024-08-13 16:51:22 +0900

    fix that pdf.worker.min.mjs is 404

--- src/pages/PDFTest.tsx
+++ src/pages/PDFTest.tsx
@@ -6,7 +6,10 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import { useMyStore } from "../store";
 
-pdfjs.GlobalWorkerOptions.workerSrc = "../../node_modules/pdfjs-dist/build/pdf.worker.min.mjs";
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+    "../../node_modules/pdfjs-dist/build/pdf.worker.min.mjs",
+    import.meta.url
+).toString();
 
 const options = {
     cMapUrl: "/cmaps/",
