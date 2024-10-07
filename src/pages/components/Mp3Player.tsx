ca29c79c07cf3267f950ff25b7da6ce7ce23ae36
Bs-Kevin <z.kyaw@bravestudio.co.jp>
2024-10-07 18:33:43 +0900

    update mp3 url

--- src/pages/components/Mp3Player.tsx
+++ src/pages/components/Mp3Player.tsx
@@ -20,7 +20,7 @@ const MP3Player = (): ReactElement => {
     // This useEffect simulates fetching or generating the 1GB MP3 file.
     useEffect(() => {
         const createLargeMp3 = (): void => {
-            const url = "https://objects.githubusercontent.com/github-production-release-asset-2e65be/833398706/5aaf9581-3204-468f-9d33-db9cfb4d0ecb?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=releaseassetproduction%2F20241007%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20241007T091927Z&X-Amz-Expires=300&X-Amz-Signature=0d6970be8bad0396fc0a27825e39d8034b447e8238d9a5567a273f907d9fbbe2&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3Dlarge-fake.mp3&response-content-type=application%2Foctet-stream";
+            const url = "https://github.com/Bravestudio-Inc-JP/PWASample/releases/download/fake-large-mp3/large-fake.mp3";
             // blob from url
             fetch(url).then(async (r) => {
                 const blob = await r.blob();
